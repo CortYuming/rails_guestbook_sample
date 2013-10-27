515e0959287631399628362ecc05a0c88f1ca387
Takashi ITO <cortyuming@gmail.com>
2013-10-27 16:08:34 +0900

    Add create in controller

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,6 +1,19 @@
 class GreetingsController < ApplicationController
   def index
     @greetings = Greeting.all.order("created_at DESC")
+    @greeting = Greeting.new
+  end
+
+  def create
+    @greeting = Greeting.new(greeting_params)
+
+    respond_to do |format|
+      if @greeting.save
+        format.html { redirect_to greetings_path }
+      else
+        format.html { render action: 'index' }
+      end
+    end
   end
 
   private
