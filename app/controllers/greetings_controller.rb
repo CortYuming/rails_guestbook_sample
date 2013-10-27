49176cd1065a6e8ee5a60f8e3d4572119da7dd45
Takashi ITO <cortyuming@gmail.com>
2013-10-27 15:57:30 +0900

    Add index and private method in controller

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,4 +1,10 @@
 class GreetingsController < ApplicationController
   def index
+    @greetings = Greeting.all.order("created_at DESC")
+  end
+
+  private
+  def greeting_params
+    params.require(:greeting).permit(:name, :comment)
   end
 end
